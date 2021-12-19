08fe7160979bfb7e4cb66f2ce84ebac2b4d3df42
Shelgir <shelgirr@gmail.com>
2021-12-19 22:37:37 +0300

    services and experience page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import TheNavbar from "./components/TheNavbar";
+import Experiences from "./pages/Experiences";
+import Services from "./pages/Services";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/experience" element={<Experiences />}></Route>
+        <Route path="/services" element={<Services />}></Route>
       </Routes>
     </div>
   );
