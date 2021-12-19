2a1749ffc23ff4f109cf598eb08b21125f909be9
Ranj Jalal <ranj.jalal97@gmail.com>
2021-12-19 22:33:37 +0300

    about us, home page, and footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import TheNavbar from "./components/TheNavbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
+
+      <Footer />
     </div>
   );
 }
