ce24cd259511a819e9052070eb22d5c3b342af58
Syar Sardar <syars66@gmail.com>
2021-12-20 12:35:40 +0300

    Git checkout problem solve

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 // import themeReducer from "./features/themeSlice";
 import { appApiSlice } from "./services/app.api";
 import userSlice from "./features/userSlice";
-
+//  slaw law pyawa
 export const store = configureStore({
   reducer: {
     user: userSlice,
