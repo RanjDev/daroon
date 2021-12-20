a88f8ebdf561272c16b4b358edd798b96118939c
Ranj Jalal <ranj.jalal97@gmail.com>
2021-12-20 13:40:44 +0300

    login route added to navbar

--- src/components/TheNavbar.js
+++ src/components/TheNavbar.js
@@ -106,7 +106,20 @@ export default function TheNavbar() {
                           </NavLink>
                         )}
                       </Menu.Item>
-
+                      <Menu.Item>
+                        {({ active }) => (
+                          <NavLink
+                            to="/login"
+                            className={({ isActive }) =>
+                              isActive
+                                ? "bg-gray-100 block px-4 py-2 text-sm text-gray-700"
+                                : "block px-4 py-2 text-sm text-gray-700"
+                            }
+                          >
+                            Login
+                          </NavLink>
+                        )}
+                      </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
                           <button
